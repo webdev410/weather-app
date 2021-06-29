8490aa6483cf6d3f73ad6e7330f56d14d1786cb6
Andrew Keiser <webdev410@gmail.com>
2021-06-28 17:35:02 -0700

    submit is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,37 +3,66 @@
 // API Key - fb3697a89b0dcdb9ac99c595bc4f441c
 
 
-var tableBody = document.getElementById('repo-table');
-var fetchButton = document.getElementById('fetch-button');
+var weatherFormEl = document.querySelector('#weatherForm')
+var cityInputEl = document.querySelector('#cityInput')
+var cityContainer = document.querySelector('#city-container')
+var citySearchTerm = document.querySelector('#city-search-term')
 
-function getApi() {
-       // fetch request gets a list of all the repos for the node.js organization
-       var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
+var formSubmit = function (event) {
+       event.preventDefault();
+       var cityInput = cityInputEl.value.trim();
+       // console.log('cityInput: ', cityInput)
 
-       fetch(requestUrl)
-              .then(function (response) {
-                     return response.json();
-              })
-              .then(function (data) {
-                     console.log(data)
-                     //Loop over the data to generate a table, each table row will have a link to the repo url
-                     for (var i = 0; i < data.length; i++) {
-                            // Creating elements, tablerow, tabledata, and anchor
-                            var createTableRow = document.createElement('tr');
-                            var tableData = document.createElement('td');
-                            var link = document.createElement('a');
+       
+       if (cityInput) {
+              getUserCity(cityInput);
+              cityContainer.textContent = '';
+              cityInputEl.value = '';
 
-                            // Setting the text of link and the href of the link
-                            link.textContent = data[i].html_url;
-                            link.href = data[i].html_url;
+             
 
-                            // Appending the link to the tabledata and then appending the tabledata to the tablerow
-                            // The tablerow then gets appended to the tablebody
-                            tableData.appendChild(link);
-                            createTableRow.appendChild(tableData);
-                            tableBody.appendChild(createTableRow);
-                     }
-              });
+       } else {
+              alert('Please enter a valid city name');
+       }
 }
 
-fetchButton.addEventListener('click', getApi);
+
+var getUserCity = function (cityInput) {
+       var apiUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=fb3697a89b0dcdb9ac99c595bc4f441c'
+
+       fetch(apiUrl)
+              .then(function (response) {
+                     if (response.ok) {
+                            console.log("response OK. response = ", response);
+                            response.json().then(function (cityInput) {
+                                   console.log("success", cityInput);
+                                   displayCity('cityInput', cityInput);
+                            });
+                     } else {
+                            alert('Error: ' + response.statusText);
+                     }
+              })
+              .catch(function (error) {
+                     alert('Unable to connect to Weather API');
+              });
+};
+
+
+weatherFormEl.addEventListener('submit', formSubmit);
+
+
+
+//        GIVEN a weather dashboard with form inputs
+//        WHEN I search for a city
+//            THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+//        WHEN I view current weather conditions for that city
+//            THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//        WHEN I view the UV index
+//        THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+//        
+//       WHEN I view future weather conditions for that city
+//            THEN I am presented with a 5 - day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+//       
+//           WHEN I click on a city in the search history
+//            THEN I am again presented with current and future conditions for that city
